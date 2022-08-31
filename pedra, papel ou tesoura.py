416976f7e59ca18ac93a36a1fbd6782e95bd3f36
Teofilo <helyezerteofilo2@hotmail.com>
2022-08-30 21:31:15 -0300

    Segundo commit adicionando a verificação de derrotas e empates

--- pedra, papel ou tesoura.py
+++ pedra, papel ou tesoura.py
@@ -1,10 +1,10 @@
 import random
 def jogo():
     qtd_vitorias = 0
-    #qtd_derrotas = 0
-    #qtd_empates = 0
+    qtd_derrotas = 0
+    qtd_empates = 0
     cont = 0
-    escolha = input("\nDeseja jogar? s ou n: ")
+    escolha = input("Bem vindo ao jogo Pedra, Papel e Tesoura! \nDeseja jogar? s ou n: ")
     while escolha.upper() == "S":
         cont+=1
         usuario = input("\nEscolha uma jogada! 'r' para pedra, 'p' para papel e 't' para tesoura: ")
@@ -12,15 +12,15 @@ def jogo():
         print("\nO computador escolheu",computador)
         if usuario == computador:
             print("Empate!")
-            #qtd_derrotas +=1
+            qtd_derrotas +=1
         elif vencedor(usuario, computador):
             print("Você ganhou!")
-            #qtd_vitorias +=1
+            qtd_vitorias +=1
         else:
             print("Você perdeu!")
-            #qtd_empates +=1
+            qtd_empates +=1
         escolha = input("\nDeseja jogar? s ou n: ")
-    return "Partidas jogadas: "+ str(cont) + " Você ganhou " + str(qtd_vitorias) +" vez(es)!"
+    return "\nPartidas jogadas: "+ str(cont) + "\n● Você ganhou " + str(qtd_vitorias) +" vez(es)! \n● Perdeu " + str(qtd_derrotas) + "\n● Empatou " + str(qtd_empates)
     
 # r > t; t > p; p > r
 def vencedor(jogador, oponente):
